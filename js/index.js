72d2478fb1cf8c071dccda4353d0c7bb9a8df64e
Musa Maphutha <josephmaphutha17@gmail.com>
2026-04-20 18:40:57 +0200

    feat: done with fetching data from ther API

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const searchBtn = document.getElementById("search-btn")
+const searchInput = document.getElementById("movie-search")
+const renderMovies = document.getElementById("movies")
+const placeholderHtml = document.getElementById("placeholder")
+
+searchBtn.addEventListener("click", async () =>{
+
+    const searchValue = searchInput.value
+
+    const res = await fetch(`http://www.omdbapi.com/?t=${searchValue}&s=${searchValue}&apikey=65ae8504`)
+    const data = await res.json()
+    console.log(data)
+    const results = data.Search
+    let html = ""
+
+    results.forEach(element => {
+        html += `
+        <div id="movie-container">
+
+        <img src="${element.Poster}">
+
+        <div id="details ${element.imdbID}">
+
+            <h1>${element.Title}</h1>
+            <h2>${element.Year}</h2>
+            <p>${element.Type}</p>
+
+        </div>
+
+       </div>
+       <hr>
+    `
+        
+    })
+
+    placeholderHtml.style.display = "none"
+    renderMovies.innerHTML = html
+
+
+    //{
+//     "Title": "Blade Runner",
+//     "Year": "1982",
+//     "imdbID": "tt0083658",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BOWQ4YTBmNTQtMDYxMC00NGFjLTkwOGQtNzdhNmY1Nzc1MzUxXkEyXkFqcGc@._V1_SX300.jpg"
+// }
+
+// 
+})
